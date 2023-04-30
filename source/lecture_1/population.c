05c06903866178ae713f50552d1b62f4dac084d0
Adrin Amin Salehi <adrin.amin@gmail.com>
2023-04-30 13:20:06 +0000

    Fix issue at lab 1

--- source/lecture_1/population.c
+++ source/lecture_1/population.c
@@ -1,50 +1,46 @@
 #include <stdio.h>
 
-int get_starting_popultation()
-{
-    int starting_population;
-    do {
-    printf("read user input ...\n");
+int get_starting_popultation() {
+  int starting_population;
+  do {
+    printf("Start size: ");
     scanf("%d", &starting_population);
-    }while (starting_population < 9);
-    printf("your starting population is %d\n", starting_population);
-    return starting_population;
+  } while (starting_population < 9);
+  return starting_population;
 }
 
-int get_ending_popultation(int starting_population)
-{
-    int ending_population;
-    do {
-    printf("read user input ...\n");
+int get_ending_popultation(int starting_population) {
+  int ending_population;
+  do {
+    printf("End size: ");
     scanf("%d", &ending_population);
-    }while (ending_population < starting_population);
-    printf("your ending population is %d\n", ending_population);
-    return ending_population;
+  } while (ending_population < starting_population);
+  return ending_population;
 }
 
-int calculate_years(int starting, int ending)
-{
-    int years = 0;
-    double born = (double)starting / 3;
-    double died = (double)starting / 4;
-    double population = starting;
-    while(population < ending) {
-        double tmp = population + born - died;
-        years++;
-        if (tmp >= ending)
-        {
-            break;
-        }
-        population = tmp;
+int calculate_years(int starting, int ending) {
+  int years = 0;
+  double population = starting;
+
+  do {
+    int born = population / 3;
+    int died = population / 4;
+    double tmp = population + (born - died);
+    years++;
+    if (tmp >= ending) {
+      break;
     }
+    population = tmp;
+  } while (population < ending);
 
-    return years;
+  return years;
 }
 
-int main (void)
-{
-    int starting_population, ending_population;
-    starting_population = get_starting_popultation();
-    ending_population = get_ending_popultation(starting_population);
-    return 0;
+int main(void) {
+  int starting_population, ending_population, years;
+  starting_population = get_starting_popultation();
+  ending_population = get_ending_popultation(starting_population);
+  years = calculate_years(starting_population, ending_population);
+  printf("Years: %d \n", years);
+  return 0;
 }
