1cd067caa908213f855cba67fc9baf4d9cd4f5e4
Adrin Amin Salehi <adrin.amin@gmail.com>
2023-10-11 09:33:57 +0000

    Fix recover algorithm

--- source/lecture_4/problem_set4/recover/recover.c
+++ source/lecture_4/problem_set4/recover/recover.c
@@ -1,55 +1,82 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
+#include <sys/stat.h>
+#include <errno.h> // Add this line
 
 #define BLOCK_SIZE 512
 
+typedef uint8_t BYTE;
+
 int main(int argc, char *argv[])
 {
-    // Your program should accept exactly one command-line argument, the name of a forensic image from which to recover JPEGs.
+    // Check for the correct number of command-line arguments
     if (argc != 2)
     {
-        printf("Usage: ./recover image\n");
+        fprintf(stderr, "Usage: ./recover image\n");
         return 1;
     }
 
-    // If the forensic image cannot be opened for reading, your program should inform the user as much, and main should return 1.
-    FILE *file = fopen(argv[1], "r");
-    if (file == NULL)
+    const char *dirname = "images";
+    if (mkdir(dirname, 0777) && errno != EEXIST)
     {
-        printf("Could not open %s.\n", argv[1]);
+        fprintf(stderr, "Could not create directory 'images'.\n");
         return 1;
     }
 
-    unsigned char *buffer = malloc(BLOCK_SIZE);
-    int jpeg_count = 0;
+    // Open the forensic image file for reading
+    FILE *infile = fopen(argv[1], "r");
+    if (infile == NULL)
+    {
+        fprintf(stderr, "Could not open %s for reading.\n", argv[1]);
+        return 1;
+    }
+
+    // Initialize variables
+    BYTE buffer[BLOCK_SIZE];
     FILE *outfile = NULL;
+    int jpeg_count = 0;
 
-    while (fread(buffer, BLOCK_SIZE, 1, file) == BLOCK_SIZE)
+    while (fread(buffer, sizeof(BYTE), BLOCK_SIZE, infile) == BLOCK_SIZE)
     {
-        // If found, open a new JPEG
-        // If not, keep looking
-        // look for beginning of JPEG
-        if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff)
+        // Check for the start of a new JPEG
+        if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
         {
-            // If already found a JPEG, close it
+            // If we're already writing to a JPEG file, close it
             if (outfile != NULL)
             {
                 fclose(outfile);
             }
 
-            // Open a new JPEG
-            char filename[8];
-            sprintf(filename, "%03i.jpg", jpeg_count);
+            // Create a new JPEG file in the "images" directory
+            char filename[15];
+            snprintf(filename, sizeof(filename), "images/%03i.jpg", jpeg_count);
             outfile = fopen(filename, "w");
+            if (outfile == NULL)
+            {
+                fclose(infile);
+                fprintf(stderr, "Could not create %s.\n", filename);
+                return 1;
+            }
+
             jpeg_count++;
         }
 
-        // If already found a JPEG, write to it
+        // Write to the current JPEG file if one is open
         if (outfile != NULL)
         {
-            fwrite(buffer, BLOCK_SIZE, 1, outfile);
+            fwrite(buffer, sizeof(BYTE), BLOCK_SIZE, outfile);
         }
     }
+
+    // Close the last JPEG file
+    if (outfile != NULL)
+    {
+        fclose(outfile);
+    }
+
+    // Close the input file
+    fclose(infile);
 
     return 0;
 }
