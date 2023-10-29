297fb1d533d0f93d4880e20a3387dc40024ec862
Adrin Amin Salehi <adrin.amin@gmail.com>
2023-10-29 06:08:23 +0000

    Update linked list file

--- source/lecture_5/linked_list.c
+++ source/lecture_5/linked_list.c
@@ -1,54 +1,178 @@
-// Prepends numbers to a linked list, using while loop to print
-
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct node 
-{
-    int number;
-    struct node *next; 
+typedef struct node {
+  int number;
+  struct node *next;
+} node;
+
+int prependNumber(int argc, char *argv[]);
+int appendNumber(int argc, char *argv[]);
+int insertNumber(int argc, char *argv[]);
+
+int main(int argc, char *argv[]) {
+  printf("Prepend numbers:\n");
+  prependNumber(argc, argv);
+  printf("Append numbers:\n");
+  appendNumber(argc, argv);
+  printf("Insert numbers (with sorting):\n");
+  insertNumber(argc, argv);
 }
-node;
 
-int main(int argc, char *argv[])
-{
-    // Memory for numbers
-    node *list = NULL;
+int prependNumber(int argc, char *argv[]) {
+  // Memory for numbers
+  node *list = NULL;
 
-    // For each command-line argument
-    for (int i = 1; i < argc; i++)
-    {
-        // Convert argument to int
-        int number = atoi(argv[i]);
+  // For each command-line argument
+  for (int i = 1; i < argc; i++) {
+    // Convert argument to int
+    int number = atoi(argv[i]);
 
-        // Allocate node for number
-        node *n = malloc(sizeof(node));
-        if (n == NULL)
-        {
-            return 1;
+    // Allocate node for number
+    node *n = malloc(sizeof(node));
+    if (n == NULL) {
+      return 1;
+    }
+    n->number = number;
+    n->next = NULL;
+
+    // Prepend node to list
+    n->next = list;
+    list = n;
+  }
+
+  // Print numbers
+  node *ptr = list;
+  while (ptr != NULL) {
+    printf("%i\n", ptr->number);
+    ptr = ptr->next;
+  }
+
+  // Free memory
+  ptr = list;
+  while (ptr != NULL) {
+    node *next = ptr->next;
+    free(ptr);
+    ptr = next;
+  }
+
+  return 0;
+}
+
+int appendNumber(int argc, char *argv[]) {
+  // Memory for numbers
+  node *list = NULL;
+
+  // For each command-line argument
+  for (int i = 1; i < argc; i++) {
+    // Convert argument to int
+    int number = atoi(argv[i]);
+
+    // Allocate node for number
+    node *n = malloc(sizeof(node));
+    if (n == NULL) {
+      return 1;
+    }
+    n->number = number;
+    n->next = NULL;
+
+    // If list is empty
+    if (list == NULL) {
+      // This node is the whole list
+      list = n;
+    }
+
+    // If list has numbers already
+    else {
+      // Iterate over nodes in list
+      for (node *ptr = list; ptr != NULL; ptr = ptr->next) {
+        // If at end of list
+        if (ptr->next == NULL) {
+          // Append node
+          ptr->next = n;
+          break;
         }
-        n->number = number;
-        n->next = NULL;
+      }
+    }
+  }
 
-        // Prepend node to list
-        n->next = list;
-        list = n;
+  // Print numbers
+  for (node *ptr = list; ptr != NULL; ptr = ptr->next) {
+    printf("%i\n", ptr->number);
+  }
+
+  // Free memory
+  node *ptr = list;
+  while (ptr != NULL) {
+    node *next = ptr->next;
+    free(ptr);
+    ptr = next;
+  }
+
+  return 0;
+}
+
+int insertNumber(int argc, char *argv[]) {
+  // Memory for numbers
+  node *list = NULL;
+
+  // For each command-line argument
+  for (int i = 1; i < argc; i++) {
+    // Convert argument to int
+    int number = atoi(argv[i]);
+
+    // Allocate node for number
+    node *n = malloc(sizeof(node));
+    if (n == NULL) {
+      return 1;
+    }
+    n->number = number;
+    n->next = NULL;
+
+    // If list is empty
+    if (list == NULL) {
+      list = n;
     }
 
-    // Print numbers
-    node *ptr = list;
-    while (ptr != NULL)
-    {
-        printf("%i\n", ptr->number);
-        ptr = ptr->next;
+    // If number belongs at beginning of list
+    else if (n->number < list->number) {
+      n->next = list;
+      list = n;
     }
 
-    // Free memory
-    ptr = list;
-    while (ptr != NULL)
-    {
-        node *next = ptr->next;
-        free(ptr);
-        ptr = next;
+    // If number belongs later in list
+    else {
+      // Iterate over nodes in list
+      for (node *ptr = list; ptr != NULL; ptr = ptr->next) {
+        // If at end of list
+        if (ptr->next == NULL) {
+          // Append node
+          ptr->next = n;
+          break;
+        }
+
+        // If in middle of list
+        if (n->number < ptr->next->number) {
+          n->next = ptr->next;
+          ptr->next = n;
+          break;
+        }
+      }
     }
+  }
+
+  // Print numbers
+  for (node *ptr = list; ptr != NULL; ptr = ptr->next) {
+    printf("%i\n", ptr->number);
+  }
+
+  // Free memory
+  node *ptr = list;
+  while (ptr != NULL) {
+    node *next = ptr->next;
+    free(ptr);
+    ptr = next;
+  }
+
+  return 0;
 }
