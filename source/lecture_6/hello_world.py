9afade5d8c145809e5bdbc71defb05c466109da4
Adrin Amin Salehi <adrin.amin@gmail.com>
2023-12-19 16:37:16 +0000

    Starting with lecture 6

---
+++ source/lecture_6/hello_world.py
@@ -0,0 +1,9 @@
+print("hello world")
+name = input("Enter your name: ")
+print("Hello, " + name)
+print("Hello (with comma),", name)
+print(f"Hello (with format string), {name}") # format string
+
+# python types
+# int, float, str, bool, range, list, tuple, dict, set
+
